15ccac22c77aaa7751dee0999c5627bc9ae640ea
Ali Mahdiyar <alimahdiyar77@gmail.com>
2020-12-13 11:21:37 +0330

    alert token

--- components/user/scan.vue
+++ components/user/scan.vue
@@ -162,6 +162,7 @@ export default {
     tokenProccessor(token) {
       // token proccessor called by camera or input if it is called by camera it returns string if not it's an input entery
       // by CAMERA
+      alert(token);
       if (typeof token == 'string') {
         this.tableCode = token.split('?token=')[1]
       }
